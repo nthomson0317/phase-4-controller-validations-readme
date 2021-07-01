bcd79ab7a309c21d74e4064542b9ff98a029d474
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-07-01 14:52:01 -0400

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,6 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   # GET /birds
   def index
@@ -11,7 +12,7 @@ class BirdsController < ApplicationController
   def create
     bird = Bird.create(bird_params)
     render json: bird, status: :created
-  end
+end
 
   # GET /birds/:id
   def show
@@ -47,4 +48,7 @@ class BirdsController < ApplicationController
     render json: { error: "Bird not found" }, status: :not_found
   end
 
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
 end
